196866898b57e038d6d6763fba704d9903562ca1
Stanislav Perekrestov <perekrestov.stas@gmail.com>
2018-02-12 20:51:41 +0200

    add some tests

---
+++ src/test/Z.Test.EntityFramework.Plus.EF6/BatchDelete/Visitor/DetectSimpleQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Z.EntityFramework.Plus;
+
+namespace Z.Test.EntityFramework.Plus
+{
+    public partial class BatchDelete_Visitor
+    {
+        [TestMethod]
+        public void DetectSimpleQuery()
+        {
+            Expression<Func<Entity_Complex, bool>> expression = e => e.ID > 3;
+            var visitor = new BatchDeleteVisitor();
+            visitor.Visit(expression);
+            Assert.IsTrue(visitor.IsSimpleQuery);
+        }
+    }
+}
