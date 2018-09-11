8631f1bd660d4f08df39928eaae4b249cd96a2e9
Stanislav Perekrestov <perekrestov.stas@gmail.com>
2018-09-11 13:48:10 +0300

    Avoids using simple query generation logic when an update statement uses expressions
    update A
    set counter = counter + 1
    where ...

--- src/shared/Z.EF.Plus.BatchUpdate.Shared/BatchUpdate.cs
+++ src/shared/Z.EF.Plus.BatchUpdate.Shared/BatchUpdate.cs
@@ -206,6 +206,7 @@ SELECT  @totalRowAffected
             {
                 return 0;
             }
+            var updateFromQueryDictValues = ResolveUpdateFromQueryDictValues(updateFactory);
 
 #if EF5 || EF6
             if (BatchUpdateManager.IsInMemoryQuery)
@@ -233,7 +234,6 @@ SELECT  @totalRowAffected
             }
 
             var dbContext = query.GetDbContext();
-
 #if EF6
             if (dbContext.IsInMemoryEffortQueryContext())
             {
@@ -276,10 +276,11 @@ SELECT  @totalRowAffected
             var innerObjectQuery = objectQuery;
 
             // GET UpdateSetValues
-            var values = GetInnerValues(query, updateFactory, entity);
+            var values = GetInnerValues(query, updateFactory, entity, updateFromQueryDictValues);
 
             // CREATE command
-            var command = CreateCommand(innerObjectQuery, entity, values, visitor);
+            bool hasNoExpressionStatements = !updateFromQueryDictValues.Any(p => p.Value is Expression);
+            var command = CreateCommand(innerObjectQuery, entity, values, visitor, hasNoExpressionStatements);
 
             // WHERE 1 = 0
             if (command == null)
@@ -356,7 +357,7 @@ SELECT  @totalRowAffected
             var queryKeys = query;
 
             // GET UpdateSetValues
-            var values = GetInnerValues(query, updateFactory, entity);
+            var values = GetInnerValues(query, updateFactory, entity, updateFromQueryDictValues);
 
             // CREATE command
             var command = CreateCommand(queryKeys, entity, values, visitor);
@@ -395,7 +396,7 @@ SELECT  @totalRowAffected
         /// <param name="query">The query.</param>
         /// <param name="entity">The schema entity.</param>
         /// <returns>The new command to execute the batch operation.</returns>
-        internal DbCommand CreateCommand<T>(ObjectQuery query, SchemaEntityType<T> entity, List<Tuple<string, object>> values, BatchUpdateVisitor visitor)
+        internal DbCommand CreateCommand<T>(ObjectQuery query, SchemaEntityType<T> entity, List<Tuple<string, object>> values, BatchUpdateVisitor visitor, bool hasNoExpressionStatements)
         {
             var objectParameters = values.Where(x => x.Item2 is ObjectParameter);
             values = values.Except(objectParameters).ToList();
@@ -470,8 +471,8 @@ SELECT  @totalRowAffected
 
                 columnKeys.Add(mappingProperty.ColumnName);
             }
-
-
+            
+            bool isSimpleQuery = visitor.IsSimpleQuery && hasNoExpressionStatements;
             // GET command text template
             var commandTextTemplate =
 #if TODO
@@ -485,7 +486,7 @@ SELECT  @totalRowAffected
                 isMySql ? CommandTextTemplate_MySQL :
                 isSqlCe ? CommandTextTemplateSqlCe :
                 isSQLite ? CommandTextTemplate_SQLite :
-                visitor.IsSimpleQuery ? CommandTextNoJoinsTemplate :
+                isSimpleQuery ? CommandTextNoJoinsTemplate :
                 CommandTextTemplate;
 
             // GET inner query
@@ -937,9 +938,9 @@ SELECT  @totalRowAffected
 #endif
 
 #if EF5 || EF6
-        internal List<Tuple<string, object>> GetInnerValues<T>(IQueryable<T> query, Expression<Func<T, T>> updateFactory, SchemaEntityType<T> entity) where T : class
+        internal List<Tuple<string, object>> GetInnerValues<T>(IQueryable<T> query, Expression<Func<T, T>> updateFactory, SchemaEntityType<T> entity, Dictionary<string, object> updateFromQueryDictValues) where T : class
 #elif EFCORE
-        public List<Tuple<string, object>> GetInnerValues<T>(IQueryable<T> query, Expression<Func<T, T>> updateFactory, IEntityType entity) where T : class
+        public List<Tuple<string, object>> GetInnerValues<T>(IQueryable<T> query, Expression<Func<T, T>> updateFactory, IEntityType entity, Dictionary<string, object> updateFromQueryDictValues) where T : class
 #endif
         {
 #if EF5 || EF6
@@ -1005,7 +1006,7 @@ SELECT  @totalRowAffected
 
 #endif
             // GET updateFactory command
-            var values = ResolveUpdateFromQueryDictValues(updateFactory); 
+            var values = updateFromQueryDictValues;
             var destinationValues = new List<Tuple<string, object>>();
 
             int valueI = -1;
