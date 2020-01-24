24800bb1f138302a2176de8c8fcf35ae03e3cc1c
David Nascimento <david.nascimento@britishcouncil.org>
2020-01-24 12:05:15 +0100

    fix: add support for queries with Take() in BatchUpdate (EFCore)

--- src/shared/Z.EF.Plus.BatchUpdate.Shared/BatchUpdate.cs
+++ src/shared/Z.EF.Plus.BatchUpdate.Shared/BatchUpdate.cs
@@ -1455,6 +1455,10 @@ SELECT  @totalRowAffected
 
                     var valueSql = commandText.Substring(6, pos);
 #endif
+					if (valueSql.Trim().StartsWith("TOP") && valueSql.IndexOf(")") != -1)
+					{
+						valueSql = valueSql.Substring(valueSql.IndexOf(")") + 1);
+					}
 
                     valueSql = valueSql.Trim();
 
