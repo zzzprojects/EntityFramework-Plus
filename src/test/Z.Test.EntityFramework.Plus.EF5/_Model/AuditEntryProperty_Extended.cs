7d49fef32b5843eba4a2da959a1ed1a2b8f32aa1
TudorVieru <tudor.v@soft-genius.com>
2024-01-17 14:34:43 +0200

    removed .net framework projects and dependencies

--- src/test/Z.Test.EntityFramework.Plus.EF5/_Model/AuditEntryProperty_Extended.cs
+++
@@ -1,9 +0,0 @@
-﻿using Z.EntityFramework.Plus;
-
-namespace Z.Test.EntityFramework.Plus
-{
-    public class AuditEntryProperty_Extended : AuditEntryProperty
-    {
-        public string ExtendedValue { get; set; }
-    }
-}
--- src/test/Z.Test.EntityFramework.Plus.EF6/_Model/AuditEntry_Extended.cs
+++
@@ -1,9 +0,0 @@
-﻿using Z.EntityFramework.Plus;
-
-namespace Z.Test.EntityFramework.Plus
-{
-    public class AuditEntry_Extended : AuditEntry
-    {
-        public string ExtendedValue { get; set; }
-    }
-}
